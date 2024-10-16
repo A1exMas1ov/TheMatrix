de2e94a81bd7f53bc3c3d8223231cb9ada8e907f
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2024-10-16 13:07:17 +0300

    Destroy and createMatrix

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+int createMatrix(size_t N, size_t n);
+void destroy(int ** matrix, sixe_t m, size_t n);
+
 int main()
 {
   size_t M = 0, N = 0;
   std::cin >> M >> N;
   std::cout << M << " " << N << "\n";
+  int ** matrix = createMatrix(M,N);
+  destroy(matrix, M, N);
 }
+
+
+int createMatrix(size_t N, size_t n);
